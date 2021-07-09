e13541dff8678e35b8b77196e34e0e526493df9e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-08 21:57:53 -0400

    Update and rename 05-scanner.java to 05-scanner-print.java

--- 01-week1/05-scanner-print.java
+++ 01-week1/05-scanner-print.java
@@ -7,5 +7,9 @@ public class ScannerTest {
     String name = scanner.nextLine();   //input as string
 //  int name = scanner.nextInt();   //input as int
     System.out.println("Hello, " + name);
+    System.out.printf("Hello, %s", name);  //printf is for formatted print, using ',' instead of '+'
   }
 }
+
+%s - string
+%d - digit
