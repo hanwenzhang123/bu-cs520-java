c74f1633bcde6af6bff97c633171d599049d84e9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-10 11:33:07 -0400

    Update 05-scanner-printf.java

--- 01-week1/05-scanner-printf.java
+++ 01-week1/05-scanner-printf.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ScannerTest {
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-    System.out.print("What is your name? ");
+    System.out.println("What is your name? ");
     String name = scanner.nextLine();   //input as string
 //  int name = scanner.nextInt();   //input as int
     System.out.println("Hello, " + name);
