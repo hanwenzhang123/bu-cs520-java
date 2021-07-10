232d3f0fe42657ab22adc9589aab298f16143dfc
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-10 10:17:06 -0400

    Update 00-071021-note.java

--- 01-week1/00-071021-note.java
+++ 01-week1/00-071021-note.java
@@ -1,1 +1,19 @@
-
+logical operation
+- the if keyword performs a truth test
+  if (x==4) //... then do something
+- the truth test ccan be a negative testt
+  if(x=!4)
+  if(!(x==4))
+! - opposite value of a booleaen
+&& - AND
+|| - OR
+  if((x==4 && y<12) || z>=2)
+the block of code is within {}, only execute if the condition is true
+  {} optional if only one thing exist in the next line
+  
+else - otherwise
+  if (x==4) {
+   x=7;
+  } else if (x>=20){
+   x=38;
+  }
