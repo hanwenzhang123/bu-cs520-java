73e986f136797f82951a3bd8be17b324ed27f1bb
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-12 22:40:59 -0400

    Update Age.java

--- assignments/Age-POP/Age.java
+++ assignments/Age-POP/Age.java
@@ -1,23 +1,12 @@
 import java.util.Scanner; 
-import java.time.LocalDateTime; 
 
-public class Age{    
+public class Main{    
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
         
         System.out.println("Enter your year of birth:");
         int yearOfBirth = scanner.nextInt(); 
-
-        scanner.nextLine();
-
-        System.out.println("Enter your month of birth:");
-        int monthOfBirth = scanner.nextInt(); 
-
-        scanner.nextLine();
-
-        System.out.println("Enter your date of birth:");
-        int dateOfBirth = scanner.nextInt(); 
 
         scanner.nextLine();
 
@@ -31,8 +20,7 @@ public class Age{
     }
 
     public static void age(String name, int yearOfBirth){
-        LocalDateTime year = LocalDateTime.getYears();  
-        int age = (int) year - yearOfBirth;
+        int age = 2021 - yearOfBirth;
         int daysOfLife = age * 365;
         System.out.println("Your name is " + name + ", and you are " + age + " years old.\n" + 
                             "You have lived in the world for over " + daysOfLife + " days."); 
