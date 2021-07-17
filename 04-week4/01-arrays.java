0a1e713b0d9b030f118a8ca9dba5f2d27a56de5b
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-17 16:46:51 -0400

    Update and rename 01- to 01-arrays.java

---
+++ 04-week4/01-arrays.java
@@ -0,0 +1,17 @@
+Java provides a data type called an array that allows us to define a single variable that has multiple values of the same type. 
+  These values, called elements, may be accessed through an index value that represents the position of the element within the array. 
+  Thus, the elements are also called indexed variables.
+
+To define an array variable, we need:
+- The type of the elements, called the base type
+- The name of the array (any legal identifier)
+- The number of elements in the array, called the size or length of the array, which could be any expression that evaluates to a non-negative integer value.
+  
+//Declaring and Creating Arrays
+Array variables are declared using square brackets: [ ].
+// Declare an array of integer values
+int[] salaries;
+
+  
+  
+  
