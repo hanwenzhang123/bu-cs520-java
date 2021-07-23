fb42c76dd5bd3d547b48b2b46f2234021690896f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-23 14:26:02 -0400

    Add files via upload

---
+++ assignments/week3-TeamTest-input-output/Player.java
@@ -0,0 +1,56 @@
+/**
+ * Player.java (Assignment 3, Part 2)
+ * @author Hanwen Zhang
+ */
+
+package hw3.part2;
+
+import java.util.Random;
+
+public class Player {
+
+    private int number;
+    private String name;
+    private String position;
+    private String year;
+
+    public Player(String name) {    //single constructor - name
+        this.name = name;
+    }
+
+    public double computeScoringAverage(){  //returns a random double value
+        Random random = new Random();   //using the package Random
+        double r = random.nextDouble();     //for double value greater or equal to 0.0 but less than 1.0
+        double value = 10.0 * r;    // a random double value between 0.0 and 10.0.
+        return value;
+    }
+
+    public String toString() {      //return the string representation of an object instance
+        return "#" + this.number + ": " + this.name + " (" + this.position + ", " + this.year + ")";
+    }
+
+    //Getters for each variable
+    public int getNumber() {
+        return number;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public String getYear() {
+        return year;
+    }
+
+    //Setters for each variable
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
---
+++ assignments/week3-TeamTest-input-output/TeamTest.java
@@ -0,0 +1,68 @@
+/**
+ * TeamTest.java (Assignment 3, Part 2)
+ * @author Hanwen Zhang
+ */
+
+package hw3.part2;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TeamTest {
+    public static void main(String[] args) {
+        Scanner scanner = null;
+
+        try {            
+            File file = new File("src/hw3/part2/team.txt");
+            scanner = new Scanner(file);
+            double totalScoringAverage = 0.0;   //initialize with a value 0.0, but will add up for each loop
+
+            while (scanner.hasNextLine()) {     //loops while hasNextLine() returns true
+                String number = scanner.nextLine();
+                String name = scanner.nextLine();
+                String position = scanner.nextLine();
+                String year = scanner.nextLine();
+                String line = scanner.nextLine();   //the variable for storing the fifth line "-----"
+
+                totalScoringAverage += createPlayer(number, name, position, year);   //add the return double value to the variable totalScoringAverage
+            }
+
+            System.out.println("Average score: " + totalScoringAverage/10);
+
+        } catch (Exception e) {   //catch all exceptions, and print the message if caught
+            System.out.println("Something went wrong, please check!");
+        } finally {     //close the scanner
+            scanner.close();
+        }
+    }
+
+    static double createPlayer(String number, String name, String position, String year) {
+
+        int numberInt=Integer.parseInt(number); //Converts String number to an int
+
+        Player player = new Player(name);   //passing name value by using Player's String constructor
+
+        player.setNumber(numberInt);    //set number using its mutator methods
+        player.setPosition(position);   //set position using its mutator methods
+        player.setYear(year);   //set year using its mutator methods
+
+        double average = player.computeScoringAverage();    //call the player instance's method
+
+        System.out.println(player.toString()+": "+average); //print player's info
+
+        return average; //return the average value
+    }
+}
+/* output:
+#1: Alan Alex (Goalkeeper, Senior): 9.844311684821948
+#2: Bryan Bob (Right Fullback, Junior): 5.672563472366077
+#3: Charles Clark (Left Fullback, Senior): 7.872193157280449
+#4: David Davis (Center Back, Sophomore): 9.309806198000944
+#5: Edison Elvis (Defending, Freshman): 9.81611816468081
+#6: Frank Faith (Right Midfielder, Junior): 9.255517170362841
+#7: George Gerald (Central, Freshman): 1.9824888163088594
+#8: Henry Harry (Striker, Junior): 6.848399194599908
+#9: Jason John (Attacking Midfielder, Senior): 4.815538048436935
+#10: Kevin Kyle (Left Midfielder, Senior): 4.671212143856032
+Average score: 7.008814805071481
+*/
