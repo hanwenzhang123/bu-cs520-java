a613d2dd7a10bed986332ae8e133c9f3cec5a848
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-24 13:17:55 -0400

    Create 03-collection.java

---
+++ 04-week4/03-collection.java
@@ -0,0 +1,14 @@
+The following interfaces provide operations on groups of objects: Collection, List, Set, Queue, Deque and Map. 
+Some of the classes implementing these interfaces are ArrayList, LinkedList, HashSet, Stack, ArrayDeque and HashMap. 
+  
+The Collection interface is the root interface of the collection hierarchy. 
+A Collection represents a group of objects. These are known as the elements of the collection.
+  
+Arrays vs. Collections
+Arrays are normally used when the number of elements is known in advance.
+Arrays provide direct access to the elements stored in the data structure. 
+  However, insertion, removal, and lookup of objects in the array are not straightforward operations. 
+  
+The classes in the Collection hierarchy are used when the number of elements varies during the lifetime of the application. 
+  They also provide operations for insertion, removal, and lookup of objects.
+    
