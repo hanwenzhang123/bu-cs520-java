d3e7d3df77e9fc7bc2c265b1054b5f65c82b33a5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-24 15:34:22 -0400

    Create 06-sets.java

---
+++ 04-week4/06-sets.java
@@ -0,0 +1,7 @@
+The Set data structure is used to represent a collection of values without any duplicate elements. The Set data structure is used to represent a collection of values without any duplicate elements. 
+  
+HashSet: Elements are stored in a hash table for fast implementation. There is no guarantee as to the order of the elements in the set.
+TreeSet: Elements are stored internally in a tree data structure, which guarantees logarithmic access time for addition, removal and retrieval of elements. The elements in the set are ordered based on their values.
+LinkedHashSet: Elements are stored in a hash table with a linked list. The elements are ordered based on the order in which they are inserted.
+
+  
